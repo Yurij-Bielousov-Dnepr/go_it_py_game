98625b5ad91db1e2d572befdfb76e3bb96542b92
YurijBielousovDnepr <gakayur@gmail.com>
2024-11-23 21:05:22 +0200

    3 version

--- main3.py
+++ main3.py
@@ -1,4 +1,6 @@
-import pygame, random, os
+import pygame
+import random
+import os
 from pygame import QUIT, K_DOWN, K_UP, K_LEFT, K_RIGHT
 pygame.init()
 FPS = pygame.time.Clock()
@@ -10,12 +12,13 @@ COLOR_BLUE = (0, 0, 255)
 COLOR_RED = (255, 0, 0)
 player_size = (20, 20)
 main_display = pygame.display.set_mode((WIDTH, HEIGHT))
-bg = pygame.transform.scale(pygame.image.load('\pic\background.png'),( WIDTH,HEIGHT))
-bg_X1=0
+bg = pygame.transform.scale(pygame.image.load(
+    '\pic\background.png'), (WIDTH, HEIGHT))
+bg_X1 = 0
 bg_X2 = bg.get_width()
-bg_move=3 
-IMAGE_PATH='goose'
-PLAYER_IMAGE=os.listdir(IMAGE_PATH)
+bg_move = 3
+IMAGE_PATH = 'goose'
+PLAYER_IMAGE = os.listdir(IMAGE_PATH)
 player = pygame.Surface(player_size)
 player.fill(COLOR_WHITE)
 player_rect = player.get_rect()
@@ -25,8 +28,9 @@ player_move_up = [0, -1]
 player_move_left = [-1, 0]
 enemyes = []
 bonuses = []
-score=0
-image_index=0
+score = 0
+image_index = 0
+
 
 def create_enemy():
     enemy_size = (30, 30)
@@ -35,13 +39,17 @@ def create_enemy():
     enemy_rect = pygame.Rect(WIDTH, random.randint(0, HEIGHT), *enemy_size)
     enemy_move = [random.randint(-6, -1), 0]
     return [enemy, enemy_rect, enemy_move]
+
+
 def create_bonus():
     bonus_size = (15, 15)
     bonus = pygame.Surface(bonus_size)
     bonus.fill(COLOR_RED)
-    bonus_rect = pygame.Rect(random.randint(0, WIDTH),0 , *bonus_size) 
-    bonus_move = [0,random.randint (1, 4)]  
+    bonus_rect = pygame.Rect(random.randint(0, WIDTH), 0, *bonus_size)
+    bonus_move = [0, random.randint(1, 4)]
     return [bonus, bonus_rect, bonus_move]
+
+
 CREATE_ENEMY = pygame.USEREVENT+1
 pygame.time.set_timer(CREATE_ENEMY, 1500)
 CREATE_BONUS = pygame.USEREVENT+2
@@ -59,8 +67,8 @@ while playing:
         if event.type == CREATE_BONUS:
             bonuses.append(create_bonus())
         if event.type == CHANGE_IMAGE:
-            player= pygame.image.load()
-    #main_display.fill(COLOR_BLACK)
+            player = pygame.image.load()
+    # main_display.fill(COLOR_BLACK)
     bg_X1 -= bg_move
     bg_X2 -= bg_move
     if bg_X1 < bg.get_width():
@@ -83,8 +91,8 @@ while playing:
         bonus[1] = bonus[1].move(bonus[2])
         main_display.blit(bonus[0], bonus[1])
         if bonus[1].bottom >= HEIGHT or bonus[1].top >= HEIGHT:
-             None
-        
+            None
+
     main_display.blit(player, player_rect)
     pygame.display.flip()
     for enemy in enemyes:
